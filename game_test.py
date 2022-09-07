0a735b57c42c5c152f82ec423a5ca908392ec469
TheLordJesusHimself <callumblumfield08@gmail.com>
2022-09-07 18:49:13 +0100

    initial commit

---
+++ game_test.py
@@ -0,0 +1,15 @@
+#import section
+
+#setting var
+Possible_Locations = ["Woods","Potato Factory"]
+Current_Location = "Woods"
+New_Location = ""
+
+# Startup Screen
+Username = input("What shall we call you,explorer?\n")
+print ("Welcome to my game,"+Username+".")
+New_Location = input("You are currently in the "+Current_Location+"! where would you like to go?\n")
+if New_Location in Possible_Locations:
+    print("in location")
+elif New_Location not in Possible_Locations:
+    print("not in location")
