4caafcd9431c5f1963616e225893895f7c0e3ac4
Cornelius-Figgle <max@fullimage.net>
2022-09-08 18:05:22 +0100

    ascii art maybe

--- game_test.py
+++ game_test.py
@@ -8,13 +8,22 @@ Current_Location = Possible_Locations[0]
 
 #________________________________________________________________________________________________________________________________
 
-def startup():
-    # Startup Screen
+def startup(): #Startup Screen
+    print('''
+                                        _            _   
+                                        | |          | |  
+    __ _  __ _ _ __ ___   ___   ______  | |_ ___  ___| |_ 
+    / _` |/ _` | '_ ` _ \ / _ \ |______| | __/ _ \/ __| __|
+    | (_| | (_| | | | | | |  __/          | ||  __/\__ \ |_ 
+    \__, |\__,_|_| |_| |_|\___|           \__\___||___/\__|
+    __/ |                                                 
+    |___/                                                  
+    ''')
+
     Username = input('What shall we call you,explorer?\n')
     print (f'Welcome to my game, {Username}')
 
-#This asks for your location in an inifite loop
-def Ask_Location(Current_Location):
+def Ask_Location(Current_Location): #This asks for your location in an infinite loop
     New_Location = input(f'You are currently in the {Current_Location}! Where would you like to go?\n')
     if New_Location in Possible_Locations: 
         return Possible_Locations[Possible_Locations.index(New_Location)]
