732329d4aa5da688d8045563a4058ce63f73cba6
TheLordJesusHimself <callumblumfield08@gmail.com>
2022-09-08 17:51:01 +0100

    game broke

--- game_test.py
+++ game_test.py
@@ -7,7 +7,7 @@ Possible_Locations = ["Woods","Factory"]
 Current_Location = "Woods"
 New_Location = ""
 
-#This asks for your location
+#This asks for your location in an inifite loop
 def Ask_Location():
     New_Location = input("You are currently in the "+Current_Location+"! where would you like to go?\n")
     if New_Location in Possible_Locations:
