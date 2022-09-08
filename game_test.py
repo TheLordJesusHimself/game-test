6fc053aff0adce33da0545555999a167c72d617d
TheLordJesusHimself <callumblumfield08@gmail.com>
2022-09-08 16:49:39 +0100

    fight me at  BS24 7AY

--- game_test.py
+++ game_test.py
@@ -1,17 +1,19 @@
 #import section
 
-#max harrison is gay
-
-#setting var
+#random variables
+Nothing = ""
+#location variables
 Possible_Locations = ["Woods","Factory"]
 Current_Location = "Woods"
 New_Location = ""
+
 
 # Startup Screen
 Username = input("What shall we call you,explorer?\n")
 print ("Welcome to my game,"+Username+".")
 New_Location = input("You are currently in the "+Current_Location+"! where would you like to go?\n")
 if New_Location in Possible_Locations:
-    print("in location")
-elif New_Location not in Possible_Locations:
-    print("not in location")
+    Current_Location = New_Location
+    New_Location = None
+print ("Current Location =",Current_Location)
+print ("New_Location =",New_Location)
