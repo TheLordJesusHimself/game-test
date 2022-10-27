64c5bc0b95e29cc31ed8c24240915d742c6939ee
TheLordJesusHimself <callumblumfield08@gmail.com>
2022-10-27 11:51:13 +0100

    more case issues

--- game_test.py
+++ game_test.py
@@ -17,7 +17,7 @@ def startup() -> None: #Startup Screen
 def Ask_Location(Current_Location) -> str | None: #This asks for your location
     New_Location = input(f'\nYou are currently in the {Current_Location.strip()}! Where would you like to go?\n >\t') # `Current_Location.strip()` just removes trailing whitepace
     if New_Location.lower() in Possible_Locations: #english syntax
-        return Possible_Locations[Possible_Locations.index(New_Location)] #lookup new location in list, return value at the point (should be same as new location)
+        return Possible_Locations[Possible_Locations.index(New_Location.lower())] #lookup new location in list, return value at the point (should be same as new location)
     else: 
         print('Not a valid answer')
         Ask_Location(Current_Location)
