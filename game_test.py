c90f413fb5903b26a871c4efbfa3c1f128446efe
TheLordJesusHimself <callumblumfield08@gmail.com>
2022-10-27 11:58:23 +0100

    fixed input case

--- game_test.py
+++ game_test.py
@@ -3,7 +3,7 @@ import os #best module, change my mind
 #________________________________________________________________________________________________________________________________
 
 #setup globals
-Possible_Locations = ['woods', 'factory']
+Possible_Locations = ['woods', 'factory', 'heaven']
 Current_Location = Possible_Locations[Possible_Locations.index('woods')] #get location of value `'Woods'` in list
 
 #________________________________________________________________________________________________________________________________
@@ -12,6 +12,10 @@ def startup() -> None: #Startup Screen
     print('                                       _            _   \n                                      | |          | |  \n  __ _  __ _ _ __ ___   ___   ______  | |_ ___  ___| |_ \n / _` |/ _` | \'_ ` _ \ / _ \ |______| | __/ _ \/ __| __|\n| (_| | (_| | | | | | |  __/          | ||  __/\__ \ |_ \n \__, |\__,_|_| |_| |_|\___|           \__\___||___/\__|\n  __/ |                                                 \n |___/                                                  \n')
     #ascii art^^
     Username = input('What shall we call you, explorer?\n >\t') #creates little arrow to indicate input
+    Username = Username.lower().split(' ')
+    for string in Username:
+        Username[Username.index(string)] = string.capitalize()
+    Username = ' '.join(Username)
     print (f'Welcome to my game, {Username}') #fstrings
 
 def Ask_Location(Current_Location) -> str | None: #This asks for your location
