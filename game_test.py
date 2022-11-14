816d61d5a0b75b5aa2b00fa43a1eaf7ee36f4375
Cornelius-Figgle <max@fullimage.net>
2022-11-14 18:59:32 +0000

    removed illegal cod3

--- game_test.py
+++ game_test.py
@@ -30,7 +30,6 @@ def Ask_Location(Current_Location: str) -> str | None: #This asks for your locat
 		return Possible_Locations[Possible_Locations.index(New_Location.lower())] #lookup new location in list, return value at the point (should be same as new location)
 	else: 
 		print('Not a valid answer')
-		#Ask_Location(Current_Location) #redo the function (mildly illegal code)
 		return Current_Location
 
 def main(Current_Location: str) -> None: #had to pass var through
